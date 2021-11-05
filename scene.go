c220b5ddf5e825abd0e8c6c7efa0dda0bfec2712
wanghj <wang@normee.co.jp>
2021-11-05 19:10:25 +0900

    add scene list cmd

--- scene.go
+++ scene.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/andreykaipov/goobs/api/requests/scenes"
@@ -26,6 +27,13 @@ var (
 			return switchScene(strings.Join(args, " "))
 		},
 	}
+	listSceneCmd = &cobra.Command{
+		Use:   "list",
+		Short: "List all scene",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return listScenes()
+		},
+	}
 )
 
 func switchScene(scene string) error {
@@ -36,7 +44,24 @@ func switchScene(scene string) error {
 	return err
 }
 
+func listScenes() error {
+	{
+		resp, err := client.Scenes.GetSceneList()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("Scene List")
+		fmt.Println("===============")
+		for _, v := range resp.Scenes {
+			fmt.Println(v.Name)
+		}
+	}
+	return nil
+}
+
 func init() {
 	sceneCmd.AddCommand(switchSceneCmd)
+	sceneCmd.AddCommand(listSceneCmd)
 	rootCmd.AddCommand(sceneCmd)
 }
